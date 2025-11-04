8ab50d2f03b3d320ed1aa437341e2f07224186a0
Maria Eduarda <mariaeduardareistds2.senai@gmal.com>
2025-11-04 16:22:44 -0300

    feat: create header initial component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,9 +3,6 @@ import "./globals.css";
 export const metadata = {
     title: "MarketCal",
     description: "Projeto final de uma agência de marketing digital para ajudar sua equipe de social media a gerenciar seus clientes",
-    icons: {
-        icon: "/icons/favicon.ico",
-    },
 };
 
 export default function RootLayout({ children }) {
